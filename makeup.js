c947eaf18064ae983bec7987897eb1036292b19e
Anil <kanilreddy867@gmail.com>
2022-12-15 21:19:35 +0530

    index>signup finished

--- makeup.js
+++ makeup.js
@@ -1,13 +1,16 @@
+let bag=[]
 let container = document.querySelector('.products-container')
 
 fetch('https://63999e5329930e2bb3d93298.mockapi.io/makeup-products')
 .then((fromResolve) => fromResolve.json())
 .then((data) =>{
-    console.log(data)
+    // console.log(data)
+    bag = data
     displayCard(data)
 })
 
 function displayCard(data){
+    container.innerHTML = null;
     data.forEach(e => {
         let divs = document.createElement('div')
         divs.setAttribute('id','each-product')
@@ -23,7 +26,7 @@ function displayCard(data){
         name.textContent = e.name
 
         let price = document.createElement('p')
-        price.textContent = e.price
+        price.textContent = '$' + e.price
 
         let cartBtn = document.createElement('button')
         cartBtn.textContent = 'Add to cart'
@@ -31,4 +34,33 @@ function displayCard(data){
         divs.append(image,name,price,cartBtn)
         container.append(divs)
     })
+}
+
+function search(){
+    event.preventDefault()
+    let a = document.querySelector('#search').value 
+    // console.log(a)
+    let searchData = bag.filter(function (e) {
+        return e.name.toLowerCase().includes(a.toLowerCase())
+    })
+    displayCard(searchData)
+}
+
+function sortbyname(){
+    let selected = document.querySelector('#option-sort').value 
+    if(selected == "Low To High"){
+        bag.sort((a,b) => a.price-b.price)
+    }
+    if(selected == "High To Low"){
+        bag.sort((a,b) => b.price-a.price)
+    }
+    console.log(bag)
+    displayCard(bag)
+}
+
+function nav_register(){
+    location.assign('./signup.html')
+}
+function nav_login(){
+    location.assign('./login.html')
 }
