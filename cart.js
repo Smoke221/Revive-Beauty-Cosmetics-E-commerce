6a78268e7e0d2330158cf826c80ca983a4dbfe0d
Anil <kanilreddy867@gmail.com>
2022-12-16 22:23:24 +0530

    cart,payment,and few changes

---
+++ cart.js
@@ -0,0 +1,70 @@
+let getData = JSON.parse(localStorage.getItem('products-in-cart')) || []
+let container = document.querySelector('#products')
+let count = 0;
+let quantity = 1;
+
+function displayCard(data) {
+    container.innerHTML = []
+    data.forEach((e, index) => {
+        let divs = document.createElement('div')
+        divs.setAttribute('id', 'cart-prod-cont')
+        count++
+        document.querySelector('#cart-count').textContent = count
+
+        let image = document.createElement('img')
+        image.setAttribute('src', e.image)
+        image.setAttribute('id', 'img')
+
+        let name = document.createElement('p')
+        name.textContent = e.name
+
+        let price = document.createElement('p')
+        price.textContent = '$' + e.price
+
+        let divs2 = document.createElement('div')
+        divs2.setAttribute('id', 'quantity-info')
+
+        let qty = document.createElement('p')
+        qty.textContent = quantity
+
+        let qtyDec = document.createElement('button')
+        qtyDec.textContent = '-'
+        qtyDec.addEventListener('click', () => {
+            if (quantity > 1) {
+                quantity--;
+                qty.textContent = quantity
+            }
+        })
+
+        let qtyInc = document.createElement('button')
+        qtyInc.textContent = '+'
+        qtyInc.addEventListener('click', () => {
+            if (quantity >= 1) {
+                quantity++;
+                qty.textContent = quantity
+            }
+        })
+
+        let removeBtn = document.createElement('button')
+        removeBtn.setAttribute('id','remove-from-cart')
+        removeBtn.textContent = 'X'
+        removeBtn.addEventListener('click', () => {
+            count--;
+            document.querySelector('#cart-count').textContent = count
+
+            getData.splice(index, 1)
+            localStorage.setItem('products-in-cart', JSON.stringify(getData))
+            displayCard(getData)
+        })
+        // console.log(count);
+        let sub_total = document.createElement('p')
+        sub_total.textContent = '$' + (quantity * e.price)
+
+        let line = document.createElement('hr')
+
+        divs2.append(qtyDec, qty, qtyInc)
+        divs.append(image, name, price, divs2, sub_total, removeBtn)
+        container.append(divs, line)
+    })
+}
+displayCard(getData)
